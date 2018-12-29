8db96ad44fdc5f0f5e4266406a8cbac8e247aa4a
StalkerSDN <38037882+StalkerSDN@users.noreply.github.com>
2018-12-29 11:34:16 +0300

    Add files via upload

---
+++ Programm.cs
@@ -0,0 +1,15 @@
+using System;
+using MyCollections;
+
+    public class Programm
+    {
+        public static void Main()
+        {
+            MyStack<int> stack = new MyStack<int>();
+            stack.Push(1);
+        stack.Push(5);
+        stack.Push(5);
+        Console.WriteLine(stack.Length);
+        Console.ReadKey();
+        }
+    }
